db652c7593e735b8f2ed39e63e8032c08b7aa538
Umar Farooq <umarone@hotmail.com>
2024-11-25 14:13:09 +0500

    success Status

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -9,5 +9,7 @@ namespace RemoteMultiSiteMobileBasedExpenseManager
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+        public int Tempo { get; set; }
     }
 }
